be8ba511b4df99ca5bc8a6715774cb35c98027e9
Marlon Zabala <marlonzbl@gmail.com>
2013-12-13 00:03:16 +0800

    house

--- CoreonMobile/src/com/itcorea/coreonmobile/ZDeleteActivityCardChooser.java
+++ CoreonMobile/src/com/itcorea/coreonmobile/ZDeleteActivityCardChooser.java
@@ -90,6 +90,8 @@ public class ZDeleteActivityCardChooser extends FragmentActivity
 				getActionBar().setTitle(mDrawerTitle);
 				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
 				//sayang tong code na mga nagawa ko :(
+				//
+				//
 			}
 		};
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
