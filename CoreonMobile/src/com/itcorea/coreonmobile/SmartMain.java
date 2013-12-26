5e176ed8d97cb75114dc9e5dd90f41be4fe38b36
Marlon <marlonzbl@gmail.com>
2013-12-26 14:47:51 +0800

    testcommit

--- CoreonMobile/src/com/itcorea/coreonmobile/SmartMain.java
+++ CoreonMobile/src/com/itcorea/coreonmobile/SmartMain.java
@@ -18,6 +18,7 @@ public class SmartMain extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
+		//hehe test commit
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.smart_main, menu);
 		return true;
