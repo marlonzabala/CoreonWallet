4aca3136bb50f0d4c5d962ce8cf96f65077aee36
Marlon <marlonzbl@gmail.com>
2014-01-03 18:03:51 +0800

    ?

--- CoreonMobile/src/com/itcorea/coreonmobile/MainSlidePageFragment.java
+++ CoreonMobile/src/com/itcorea/coreonmobile/MainSlidePageFragment.java
@@ -1,17 +1,23 @@
 package com.itcorea.coreonmobile;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.PropertyInfo;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.itcorea.coreonmobile.R;
+import android.widget.Toast;
 
 @SuppressLint("ValidFragment")
 public class MainSlidePageFragment extends Fragment
 {
-
 	public int	pageNum	= 0;
 
 	@SuppressLint("ValidFragment")
@@ -43,51 +49,4 @@ public class MainSlidePageFragment extends Fragment
 	}
 	
 	
-	
-	private final String	NAMESPACE	= "http://www.w3schools.com/webservices/";
-	private final String	URL			= "http://www.w3schools.com/webservices/tempconvert.asmx";
-	private final String	SOAP_ACTION	= "http://www.w3schools.com/webservices/CelsiusToFahrenheit";
-	private final String	METHOD_NAME	= "CelsiusToFahrenheit";
-	private String			TAG			= "PGGURU";
-	private static String	celcius;
-	private static String	fahren;
-	
-	
-	
-	public void getFahrenheit(String celsius)
-	{
-//		// Create request
-//		SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-//		// Property which holds input parameters
-//		PropertyInfo celsiusPI = new PropertyInfo();
-//		// Set Name
-//		celsiusPI.setName("Celsius");
-//		// Set Value
-//		celsiusPI.setValue(celsius);
-//		// Set dataType
-//		celsiusPI.setType(double.class);
-//		// Add the property to request object
-//		request.addProperty(celsiusPI);
-//		// Create envelope
-//		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-//		envelope.dotNet = true;
-//		// Set output SOAP object
-//		envelope.setOutputSoapObject(request);
-//		// Create HTTP call object
-//		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-//
-//		try
-//		{
-//			// Invole web service
-//			androidHttpTransport.call(SOAP_ACTION, envelope);
-//			// Get the response
-//			SoapPrimitive response = (SoapPrimitive) envelope.getResponse();
-//			// Assign it to fahren static variable
-//			fahren = response.toString();
-//		}
-//		catch (Exception e)
-//		{
-//			e.printStackTrace();
-//		}
-	}
 }
